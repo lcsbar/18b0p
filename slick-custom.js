8d79964a98e818c342ca5038927378921313aa4e
lcsbar <lcsbar63@gmail.com>
2025-02-22 21:52:39 +0900

    Create slick-custom.js

---
+++ slick-custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('.neko-img').slick({
+		autoplay: true,
+		autoplay: 6000,
+		speed: 2000,
+		dots: true,
+		// cssEase: 'ease-in-out',
+		fade: true,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+	});
+});
